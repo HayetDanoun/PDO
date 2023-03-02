f1da30c78d72badf9427e58e3604e00d8a8ef364
DANOUN Hayet <hayetdanoun@gmail.com>
2023-03-02 10:09:47 +0100

    update index.php

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <?php
     require_once '_connec.php';
-
     $pdo = new PDO(DNS,USER,PASSWORD);
-
-    $query = "SELECT * FROM friend ";
-    $statement = $pdo->query($query);
-    $tabObjFriends = $statement->fetchAll(PDO::FETCH_OBJ);
-
-    if(!empty($tabObjFriends)){
-        echo 'Voici la liste de mes amis <br>';
-        //var_dump($tabObjFriends);       
-        echo '<ul>';
-        foreach($tabObjFriends as $value){
-            echo '<li>' . $value->firstname . ' ' . $value->lastname . '</li>'; 
-        }
-        echo '</ul>';
-    }
 ?>
 <h1>Ajouter un nouveau ami</h1>
 <form method='post' action="index.php">
@@ -32,31 +17,51 @@
 <?php
     $errors = [];
     if(!empty($_POST)){
-        if(isset($_POST['firstname']) && strlen($_POST['firstname']) > 45)
-            $errors[] = 'Le champ Firstname doit faire moins de 45 caracteres';
-        if(isset($_POST['lastname']) && strlen($_POST['lastname']) > 45)
-            $errors[] = 'Le champ Lastname doit faire moins de 45 caracteres';
-
+        if(isset($_POST['firstname']) )
+                if(strlen($_POST['firstname']) > 45) $errors[] = 'Le champ Firstname doit faire moins de 45 caracteres je ne peux pas rajouter cet ami désolé';
+                if(empty($_POST['firstname']) || trim($_POST['firstname']) === "")  $errors[] = 'Le champ Firstname doit etre rempli je ne peux pas rajouter cet ami désolé';
+        if(isset($_POST['lastname']) )
+                if(strlen($_POST['lastname']) > 45) $errors[] = 'Le champ Lastname doit faire moins de 45 caracteres je ne peux pas rajouter cet ami désolé';
+                if(empty($_POST['lastname']) || trim($_POST['lastname']) === "")  $errors[] = 'Le champ Lastname doit etre rempli je ne peux pas rajouter cet ami désolé';
 
         //var_dump($errors);
         foreach($errors as $value){
             echo $value . '<br>' ;
         }
-        if(empty($error)){
+        if(empty($errors)){
 
-            $query = 'INSERT INTO friend (firstname,lastname) VALUES (:firstname,:lastname)';
+            $query = "INSERT INTO friend (firstname,lastname) VALUES (:firstname,:lastname)";
             $statement = $pdo->prepare($query);
-
+        
             $firstname = $_POST['firstname'];
             $lastname = $_POST['lastname'];
 
             $statement->bindValue(':firstname',$firstname);
             $statement->bindValue(':lastname',$lastname);
-
+    
             $statement->execute();
-
+    
             echo 'Felicitation ' . $firstname . ' ' . $lastname . ' est desormais votre ami !';
+            header("Location: /");
         }
+        else{
+            echo '<br>';
+        }
+    }
+?>
 
+<?php
+    $query = 'SELECT * FROM friend ';
+    $statement = $pdo->query($query);
+    $tabObjFriends = $statement->fetchAll(PDO::FETCH_OBJ);
+    
+    if(!empty($tabObjFriends)){
+        echo 'Voici la liste de tout vos amis amis <br>';
+        //var_dump($tabObjFriends);       
+        echo '<ul>';
+        foreach($tabObjFriends as $value){
+            echo '<li>' . $value->firstname . ' ' . $value->lastname . '</li>'; 
+        }
+        echo '</ul>';
     }
 ?>
